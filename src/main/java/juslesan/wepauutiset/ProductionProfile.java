c787e43256dca110bcb797f464136626d76cdd9f
Juslesan <santeri.juslenius@gmail.com>
2017-11-25 13:48:43 +0200

    heroku

--- src/main/java/juslesan/wepauutiset/ProductionProfile.java
+++ src/main/java/juslesan/wepauutiset/ProductionProfile.java
@@ -19,20 +19,15 @@ import org.apache.commons.dbcp2.BasicDataSource;
  *
  * @author santeri
  */
-@Configuration
-@Profile("production")
-public class ProductionProfile {
 
+@Profile("production")
+@Configuration
+public class ProductionProfile {
+    
     @Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-        return basicDataSource;
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        System.out.println("Hello world!");
+        return DataSourceBuilder.create().build();
     }
 }
