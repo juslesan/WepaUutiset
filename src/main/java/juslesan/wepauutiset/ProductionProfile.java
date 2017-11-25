b48a41fe0e229abbd4afc4c9ab54dc53ddaef49b
Juslesan <santeri.juslenius@gmail.com>
2017-11-25 13:00:49 +0200

    heroku

--- src/main/java/juslesan/wepauutiset/ProductionProfile.java
+++ src/main/java/juslesan/wepauutiset/ProductionProfile.java
@@ -5,12 +5,15 @@
  */
 package juslesan.wepauutiset;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import javax.sql.DataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
  *
@@ -21,9 +24,15 @@ import org.springframework.context.annotation.Profile;
 public class ProductionProfile {
 
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-         System.out.println("Hello world!");
-        return DataSourceBuilder.create().build();
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        return basicDataSource;
     }
 }
